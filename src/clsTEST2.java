f42f0a178748418e39fcf9215c2a75c47f99ebe6
Kamal_Agarwal <Kamal_Agarwal@EPUSPRIW00B1.princeton.epam.com>
2021-12-04 13:42:53 -0800

    Test2 V2

--- src/clsTEST2.java
+++ src/clsTEST2.java
@@ -4,6 +4,7 @@ public class clsTEST2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test2 ");
+		System.out.println("TEST 2 Updated");
 	}
 
 }
